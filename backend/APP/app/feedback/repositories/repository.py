6dfdfcb190089d7bab9a8921d7e73e3a4f0c0e77
ahmedhoucine <ahmed.houcine@insat.ucar.tn>
2025-08-27 09:41:59 +0100

    microservice done

--- backend/APP/app/feedback/repositories/repository.py
+++
@@ -1,7 +0,0 @@
-from feedback.domain.models.feedback import Feedback
-from shared.db import db
-
-def get_all_feedbacks():
-    return Feedback.query.all()
-
-
--- backend/APP/app/feedback/services/service.py
+++
@@ -1,5 +0,0 @@
-from feedback.repositories import repository
-import ollama  # ou un wrapper de ton choix vers Ollama Mistral
-
-def lister_feedbacks():
-    return repository.get_all_feedbacks()
